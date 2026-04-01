c6c92006dabf5f5bf1a0824d760f4f878c703bf1
damanpreet-kaur123 <155047827+damanpreet-kaur123@users.noreply.github.com>
2026-04-01 15:23:41 +0530

    Create authRoutes.js

---
+++ routes/authRoutes.js
@@ -0,0 +1,24 @@
+import {
+    forgotPassword,
+    protectedRoute,
+    publicRoute,
+    resetPassword,
+    signin,
+    signup,
+} from "../controllers/authController.js";
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.get("/protected", authMiddleware, protectedRoute);
+router.get("/public", publicRoute);
+
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
+export default router;
