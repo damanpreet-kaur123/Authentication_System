b0b4ad22a42c0938bf92e150331ef70ad542c181
damanpreet-kaur123 <155047827+damanpreet-kaur123@users.noreply.github.com>
2026-04-01 15:22:36 +0530

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    resetToken: String,
+    createdAt: { type: Date, default: Date.now },
+});
+
+const UserModelAuth = mongoose.model("UserModelAuth", userSchema);
+
+export default UserModelAuth;
